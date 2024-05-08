28fae1b8fa2ed3fc613d7289d93bc40c82763919
Kate505 <ksdelgado31@gmail.com>
2024-05-08 15:39:38 -0600

    New hero search functionality

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -11,24 +11,26 @@ export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance
   const heroImageUrl = `/assets/${ id }.jpg`;
 
   return (
-    <div className="col animate__animated animate_fadeIn">
+    <div className="col animate__animated animate__fadeIn">
       <div className="card">
 
         <div className="row no-gutters">
           <div className="col-4">
             <img src={ heroImageUrl } className="card-img" alt={ superhero }/>
           </div>
-          <div className="card-body">
-            <h5 className="card-title">{ superhero }</h5>
-            <p className="card-text">{ alter_ego }</p>
-            <CharactersByHero characters={characters} alter_ego={alter_ego}/>
-            <p className="card-text">
-              <small className="text-muted">{ first_appearance }</small>
-            </p>
-            
-            <Link to={`/hero/${ id }`}>
-              Mas...
-            </Link>
+          <div className="col-8">
+            <div className="card-body">
+              <h5 className="card-title">{superhero}</h5>
+              <p className="card-text">{alter_ego}</p>
+              <CharactersByHero characters={characters} alter_ego={alter_ego}/>
+              <p className="card-text">
+                <small className="text-muted">{first_appearance}</small>
+              </p>
+
+              <Link to={`/hero/${id}`}>
+                Mas...
+              </Link>
+            </div>
           </div>
         </div>
 
--- src/heroes/helpers/index.js
+++ src/heroes/helpers/index.js
@@ -1,2 +1,3 @@
 export * from "./getHeroById.js";
 export * from "./getHeroesByPublisher.js";
+export * from "./getHeroesByName.js";
